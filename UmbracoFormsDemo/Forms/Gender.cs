1811e289e9a58f208adbb9f7e2e2b1eb917c4a4b
Rick Mason <rick.mason@tpr.gov.uk>
2020-03-27 16:01:12 +0000

    Add custom field type. Include the App_Plugins folder to make recreating this demo easier, and to include the custom file.

---
+++ UmbracoFormsDemo/Forms/Gender.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Umbraco.Forms.Core;
+
+namespace UmbracoFormsDemo.Forms
+{
+    public class Gender : FieldType
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Gender"/> class.
+        /// </summary>
+        public Gender()
+        {
+            Id = new Guid("f384b6df-1344-4416-a4af-562c97a23844");
+            Name = "Gender";
+            Description = "Only ask if you'll use it. Call it 'Gender', not 'Sex' (unless it's medical).";
+            DataType = FieldDataType.String;
+            FieldTypeViewName = "FieldType.Gender.cshtml";
+            Icon = "icon-male-and-female";
+            HideLabel = false;
+            SupportsPrevalues = false;
+            SupportsRegex = false;
+            SortOrder = 100;
+        }
+    }
+}
